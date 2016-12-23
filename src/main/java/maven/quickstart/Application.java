ff1203578c68918e45f9ed6d6606610de72660b5
bikash <bikashmazumdar2000@gmail.com>
2016-12-23 13:49:45 +0530

    dependency injection

--- src/main/java/maven/quickstart/Application.java
+++ src/main/java/maven/quickstart/Application.java
@@ -2,9 +2,14 @@ package maven.quickstart;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 	
+	public int countWords(String s) {
+		String [] words = StringUtils.split(s,' ');
+		return (words == null ? 0 : words.length);
+	}
 	public void greet() {
 		List<String> l = new ArrayList<>();
 		
@@ -26,5 +31,6 @@ public class Application {
     	System.out.println ("Starting Application");
     	Application app = new Application();
     	app.greet();
+    	System.out.println(app.countWords("This is a book"));
     }
 }

c5407c69eb5b26bc581ccbc89fa30a8273a465fb
bikash <bikashmazumdar2000@gmail.com>
2016-12-23 13:34:34 +0530

    changing complier plugin

--- src/main/java/maven/quickstart/Application.java
+++ src/main/java/maven/quickstart/Application.java
@@ -1,6 +1,21 @@
 package maven.quickstart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+	
+	public void greet() {
+		List<String> l = new ArrayList<>();
+		
+		l.add("add");
+		
+		for (String item : l) {
+			
+			System.out.println(item);
+			
+		}
+	}
     
     public Application() {
         System.out.println ("Inside Application");
@@ -9,6 +24,7 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+    	Application app = new Application();
+    	app.greet();
     }
 }
